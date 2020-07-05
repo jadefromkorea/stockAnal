ca8ff9db79dff49b114a3de8e641d67fd1097b32
이종현 <jadefromkorea@gmail.com>
2020-07-05 22:26:41 +0900

    Initial commit

---
+++ src/main/java/com/jade/stockAnal/StockAnalApplication.java
@@ -0,0 +1,13 @@
+package com.jade.stockAnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jade/stockAnal/StockAnalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jade.stockAnal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockAnalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
